afb9fea698f80a776b4d79bbb8bdfe415b3cca2f
Amber-Pittman <amber@amberpittman.com>
2020-05-17 16:31:49 -0400

    Corrected syntax in table

--- data/migrations/20200517162425_winchesters.js
+++ data/migrations/20200517162425_winchesters.js
@@ -1,13 +1,14 @@
 
-exports.up = function(knex) {
+exports.up = async function(knex) {
   await knex.schema.createTable("winchesters", (table) => {
       table.increments()
       table.text("name").notNull()
       table.text("role").notNull()
       table.text("years_active").notNull()
       table.text("death_count").notNull()
-};
+  })
+}
 
-exports.down = function(knex) {
+exports.down = async function(knex) {
   await knex.schema.dropTableIfExists("winchesters")
-};
+}
